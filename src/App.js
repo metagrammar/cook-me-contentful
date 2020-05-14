153130400ddb38c5ad3e52ebc7ab0be17d3f5c1d
Antonio Silva <antonioesilva@gmail.com>
2020-05-14 17:16:03 +0200

    Components: Results Main, Footer & CSS main structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import ResultsMain from './components/ResultsMain'
+import Footer from './components/Footer'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+     <ResultsMain />
+     <Footer />
     </div>
   );
 }
