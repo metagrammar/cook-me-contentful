dbc25884318a11d9e81565a1ded39cf4583bf2f6
Christoph Steiner <gawen@t-online.de>
2020-05-19 17:59:38 +0200

    recipe & steps fixes

--- src/components/steps.js
+++ src/components/steps.js
@@ -3,15 +3,18 @@ import './steps.css';
 
 
 function Steps({recipeData}) {
+    var i = 0;
+
     return (
         <div className="steps" id="steps">
         {recipeData.map( step => {
             return (
             <div>
+            <h4>Step {i+=1}</h4>
             <p>{step[0]}</p>
             {step[1] === "no image"? "": <img src={step[1]} alt="delicous"></img>}
             </div> )
-        }
+            }
         )}
             
 
