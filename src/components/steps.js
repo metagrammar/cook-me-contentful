0d13854f44246b643629d14ebf6f61f54b308d54
Antonio Silva <antonioesilva@gmail.com>
2020-05-20 00:03:05 +0200

    Carousel item map(), Home cleanup, Recipe Page structure and styling

--- src/components/steps.js
+++ src/components/steps.js
@@ -3,21 +3,26 @@ import './steps.css';
 
 
 function Steps({recipeData}) {
+    
     var i = 0;
 
     return (
-        <div className="steps" id="steps">
-        {recipeData.map( step => {
-            return (
-            <div>
-            <h4>Step {i+=1}</h4>
-            <p>{step[0]}</p>
-            {step[1] === "no image"? "": <img src={step[1]} alt="delicous"></img>}
-            </div> )
-            }
-        )}
-            
-
+        <div id="steps">
+            {recipeData.map( step => {
+                return (
+                <div className="steps">
+                    <div className='steps-text'>
+                        <h4>Step {i+=1}</h4>
+                        <p>{step[0]}</p>
+                    </div>
+                    
+                    {step[1] === "no image"? "":            
+                    <div className='steps-image'> 
+                        <img src={step[1]} alt="delicous" /> 
+                    </div>}  
+                </div>
+                )}
+            )}
         </div>
     );
   }
